7540d03726a8c1ef13d905a0f4cf9152cd0a3af0
carlos-ch <carlos.tschavez@gmail.com>
2021-02-25 12:04:48 +0100

    add src folder and first phase code

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+import ContentWrapper from '../../styles/contentWrapper';
+import Logo from '../Logo';
+import Links from './Links';
+
+const StyledNavWrapper = styled(ContentWrapper)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 5rem;
+  background-color: rgb(1, 61, 85);
+`;
+
+const Navbar = () => {
+  return (
+    <StyledNavWrapper>
+      <Logo size="2rem" className="logo">
+        Super Store
+      </Logo>
+      <Links />
+    </StyledNavWrapper>
+  );
+};
+
+export default Navbar;
