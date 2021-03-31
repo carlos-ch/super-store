9c01e111a79bcf6c4681b7257c325b94aa8c9e2a
carlos-ch <carlos.tschavez@gmail.com>
2021-03-31 09:15:16 +0200

    edit button sytiling and globalStyle

--- src/styles/button.js
+++ src/styles/button.js
@@ -4,9 +4,20 @@ const Button = styled.button`
   background-color: #ffd19b;
   width: fit-content;
   padding: 0.5rem 2rem;
-  border: none;
+  border: 1px solid transparent;
   border-radius: 3px;
   cursor: pointer;
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+  &:hover {
+    color: #000;
+    background-color: #ecb87b;
+    border-color: #d29f62;
+  }
+  &:focus {
+    outline: none;
+    text-decoration: none;
+  }
 `;
 
 export default Button;
