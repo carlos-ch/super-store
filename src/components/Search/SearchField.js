8b9e126d40de9bcb07648ec6ccbb899e5007729e
carlos-ch <carlos.tschavez@gmail.com>
2021-04-01 09:39:28 +0200

    small edits to searchField component

--- src/components/Search/SearchField.js
+++ src/components/Search/SearchField.js
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import Button from "../../styles/button";
-import SearchBar from "../SearchBar";
-import searchIcon from "../../assets/svg/search.svg";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Button from '../../styles/button';
+import SearchBar from '../SearchBar';
+import searchIcon from '../../assets/svg/search.svg';
 
 const StyledForm = styled.form`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 100%;
+  width: 100vw;
+  // max-width: 100%;
   margin-bottom: 2rem;
   background-color: #eee;
   .icon {
@@ -20,19 +21,19 @@ const StyledForm = styled.form`
 const icon = <img className="icon" src={searchIcon} alt="search"></img>;
 
 const SearchField = ({ onSearch }) => {
-  const [input, setInput] = useState("");
-  console.log(input);
-  const handlerSubmit = (e) => {
+  const [input, setInput] = useState('');
+  console.log({ input });
+  const handlerSubmit = e => {
     e.preventDefault();
     onSearch(input);
     // setSearch(input);
   };
 
   return (
-    <StyledForm onSubmit={(e) => handlerSubmit(e)}>
+    <StyledForm onSubmit={e => handlerSubmit(e)}>
       <SearchBar
         onSearch={onSearch}
-        onInput={(e) => setInput(e.target.value)}
+        onInput={e => setInput(e.target.value)}
       ></SearchBar>
       <Button>{icon}</Button>
     </StyledForm>
